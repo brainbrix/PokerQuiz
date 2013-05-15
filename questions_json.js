f252f9cbd2c811f897ef571e63b66cec4cc099da
Heiko Spindler <heiko.spindler@hirnsport.de>
2013-05-15 22:26:12 +0200

    Questions as json

---
+++ questions_json.js
@@ -0,0 +1,5 @@
+
+ jsonData = [ {"id":1,"question":"Where is Paris?","answers":["France", "Belgium", "Norway", "Germany"], "category":"Geographie", "image":"images/demobild800600.png"},
+     {"id":2,"question":"Where is London?","answers":["England", "China", "Denmark", "Greece"], "category":"Geographie"},
+     {"id":3,"question":"Where is Rome?","answers":["Italy", "Brazil", "Canada", "Australia"], "category":"Geographie", "image":"images/demobild800600.png"},
+     {"id":4,"question":"Where is Berlin?","answers":["Germany", "Brazil", "Canada", "Australia"], "category":"Geographie", "image":""} ];
