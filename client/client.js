a18d92f7803eccc8323f5f7c210649b995fae7b1
Niko <niko@n-k.de>
2013-05-28 08:45:34 +0200

    changed structure in JS file and added momentJS as util library

--- client/client.js
+++ client/client.js
@@ -1,15 +1,6 @@
-getCurrentDateString = function () {
-    da = new Date();	// Create a Date Object set to the last modifed date
-    db = da.toGMTString();	// Convert to a String in "predictable formt"
-    dc = db.split(" ");	// Split the string on spaces
-    if (eval(dc[3]) < 1970) dc[3] = eval(dc[3]) + 100;	// Correct any date purporting to be before 1970
-    db = dc[1] + " " + dc[2] + " " + dc[3];	// Ignore day of week and combine date, month and year
-    return db;	// Display the result
-}
-
-Template.leaderboard.writeLog = function (thename, thescore, themessage) {
-    var d = new Date();
-    Logs.insert({zeitpunkt: getCurrentDateString() + " " + d.toLocaleTimeString(), name: thename, score: thescore, message: themessage});
+writeLog = function (thename, thescore, themessage) {
+    var dateTimeString = moment().format("YYYY-MM-DD HH:mm:ss");
+    Logs.insert({zeitpunkt: dateTimeString, name: thename, score: thescore, message: themessage});
 }
 
 Accounts.ui.config({
@@ -19,7 +10,7 @@ Accounts.ui.config({
     passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
 });
 
-Meteor.autorun(function () {
+Deps.autorun(function () {
     if (Meteor.user()) {
 
         var user = Meteor.user();
@@ -33,7 +24,7 @@ Meteor.autorun(function () {
         if (!dbuser) {
             Players.insert({name: username, score: 10});
             dbuser = Players.findOne({name: username});
-            Template.leaderboard.writeLog(dbuser.name, dbuser.score, "has registered");
+            writeLog(dbuser.name, dbuser.score, "has registered");
         }
         userId = dbuser._id;
 
@@ -42,92 +33,6 @@ Meteor.autorun(function () {
     }
 });
 
-
-Template.ranking.players = function () {
-    return Players.find({}, {limit: 10, sort: {score: -1, name: 1}});
-};
-
-Template.activities.logs = function () {
-    return Logs.find({}, { limit: 10, sort: {zeitpunkt: -1}});
-};
-
-Template.question.answers = function () {
-    return Answers.find({}, {});
-};
-
-
-Template.questions.questions = function () {
-    var count = Questions.find({}, {}).count();
-    var rp = Math.floor(Math.random() * count);
-    console.log("rp: " + rp + " count:" + count);
-    var selectedQuestion = Questions.findOne({}, { limit: 1, sort: {id: -1}, skip: rp });
-
-    var pn = Array();
-
-    for (var propName in selectedQuestion) {
-        console.log("Iterating through prop with name>", propName, "< its value is ", selectedQuestion[propName]);
-    }
-
-    for (var propName in selectedQuestion) {
-        pn.push(propName);
-        console.log("Iterating through prop with name>", propName, "< its value is ", selectedQuestion[propName]);
-    }
-
-    for (var propName in pn) {
-        console.log("Iterating through prop with name>", propName, "< its value is ", pn[propName]);
-        console.log("Iterating through prop with name>", propName, "< its value is ", selectedQuestion[ pn[propName] ]);
-    }
-
-    var theAnswers = selectedQuestion[ 'answers' ];
-    if (selectedQuestion) {
-
-        var temp = theAnswers[0];
-        var randfn = function (left, right) {
-            return Math.floor(Math.random() * 3) - 1;
-        };
-        theAnswers.sort(randfn);
-        theAnswers.sort(randfn);
-        theAnswers.sort(randfn);
-        theAnswers.sort(randfn);
-
-        selectedQuestion[ 'answers' ] = theAnswers;
-    }
-    Session.set("selected_question", selectedQuestion);
-    Session.set("correct_answer", temp);
-    Session.set("selected_answer", "");
-
-    Answers.remove({});
-
-    for (var i = 0; i < theAnswers.length; i++) {
-        Answers.insert({label: String.fromCharCode(65 + i), text: theAnswers[i] });
-        // Similar to 4 lines like:
-        // Answers.insert( {label:'A', text: theAnswers[0] } );
-    }
-
-    return q;
-};
-
-Template.answer.events({
-
-    'click': function (a) {
-        console.info(this);
-        var correct = Session.get("correct_answer");
-        var points = -5;
-        var message = " was wrong."
-
-        if (this.text === correct) {
-            points = 10;
-            message = " answered right."
-        }
-
-        Players.update(userId, {$inc: {score: points}});
-        Template.leaderboard.writeLog(username, points, message);
-
-        Session.set("selected_answer", this.text)
-
-        return false;
-    }
-});
 
 Template.questions.events({
     'click .nextQuestion': function (a) {
@@ -140,40 +45,30 @@ Template.questions.events({
     }
 });
 
-Template.questions.chosenAnswer = function () {
-    //alert( "Answer what"+what );
+Template.questions.questions = function () {
+    var count = Questions.find({}, {}).count();
+    var rp = Math.floor(Math.random() * count);
+    console.log("rp: " + rp + " count:" + count);
+    var selectedQuestion = Questions.findOne({}, { limit: 1, sort: {id: -1}, skip: rp });
 
-    /*	var myRsvp = _.find(this.rsvps, function (r) {
-     return r.user === Meteor.userId();
-     }) || {};
-     */
-    return Session.get("selected_answer") == Session.get("correct_answer");
-};
+    var theAnswers = selectedQuestion["answers"];
+    if (selectedQuestion) {
+        var temp = theAnswers[0];
+        theAnswers = _.shuffle(theAnswers);
+        selectedQuestion["answers"] = theAnswers;
+    }
+    Session.set("selected_question", selectedQuestion);
+    Session.set("correct_answer", temp);
+    Session.set("selected_answer", "");
 
-Template.questions.mix2 = function (a) {
-    if (a) {
-        console.log("mix q" + q);
-        var temp = q[0];
-
-        var randfn = function (left, right) {
-            return Math.floor(Math.random() * 3) - 1;
-        };
-
-        q.sort(randfn);
-        q.sort(randfn);
-        //	q[0].answers.sort(randfn);
-        //	q[0].answers.sort(randfn);
-
-        console.log("correct:" + q.indexOf(temp));
+    Answers.remove({});
+    for (var i = 0; i < theAnswers.length; i++) {
+        Answers.insert({label: String.fromCharCode(65 + i), text: theAnswers[i] });
     }
 };
 
-Template.player.selected = function () {
-    return userId == this._id ? "warning" : "";
-};
-
-Template.logentry.isError = function() {
-    return (this.score > 0) ? "success" : "error";
+Template.questions.chosenAnswer = function () {
+    return Session.get("selected_answer") == Session.get("correct_answer");
 };
 
 Template.questions.selected_question = function () {
@@ -184,10 +79,52 @@ Template.questions.correct_answer = function () {
     return Session.get("correct_answer");
 };
 
+Template.questions.selected_answer = function () {
+    return Session.get("selected_answer");
+};
+
 Template.question.questionselected = function () {
     return Session.get("selected_question");
 };
 
-Template.questions.selected_answer = function () {
-    return Session.get("selected_answer");
+Template.question.answers = function () {
+    return Answers.find({}, {});
+};
+
+Template.answer.events({
+    'click .answer': function (a) {
+        console.info(this);
+        var correct = Session.get("correct_answer");
+        var points = -5;
+        var message = " was wrong."
+
+        if (this.text === correct) {
+            points = 10;
+            message = " answered right."
+        }
+
+        Players.update(userId, {$inc: {score: points}});
+        writeLog(username, points, message);
+
+        Session.set("selected_answer", this.text)
+
+        return false;
+    }
+});
+
+
+Template.ranking.players = function () {
+    return Players.find({}, {limit: 10, sort: {score: -1, name: 1}});
+};
+
+Template.player.selected = function () {
+    return userId == this._id ? "warning" : "";
+};
+
+Template.activities.logs = function () {
+    return Logs.find({}, { limit: 10, sort: {zeitpunkt: -1}});
+};
+
+Template.logentry.isError = function() {
+    return (this.score > 0) ? "success" : "error";
 };
