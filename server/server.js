a6ba5cf922656cb2d83d1ad3c269d41d7ee294ed
Niko Köbler <niko.koebler@de.amadeus.com>
2013-05-29 12:53:16 +0200

    added europe information and build up questions depending on that

--- server/server.js
+++ server/server.js
@@ -13,12 +13,24 @@ Meteor.startup(function () {
         }
     }
 
-    if (Questions.find().count() < questionsJson.length) {
-        // File questions_json.js is auto parsed and loaded
-        for (var i = 0; i < questionsJson.length; i++) {
-			var q = questionsJson[i];
-            console.log("insert question: %o", q);
-            Questions.insert({id: q.id, question: q.question, answers: q.answers });
+    // File europe.js is auto parsed and loaded
+    if (Questions.find().count() < europe.length) {
+        Questions.remove({});
+        var countries = _.shuffle(europe)
+        for (var i = 0; i < countries.length; i++) {
+            var c = countries[i];
+            console.log("creating question with country %o", c);
+            var q = "What is the capital of " + c.country + "?";
+            var a = [];
+            a.push(c.capital);
+            for (int n = 1; n < 4; n++) {
+                var k = i + n;
+                if (k > countries.length) {
+                    k = k - countries.length;
+                }
+                a.push(countries[k].capital);
+            }
+            Questions.insert({id: i, question: q, answers: a});
         }
     }
 });
@@ -26,23 +38,23 @@ Meteor.startup(function () {
 // Publish the collections used on server and client
 // (declared in /common/collections.js)
 Meteor.publish("logs", function() {
-	return Logs.find({}, {limit: 10, sort: {timestamp: -1}});
+    return Logs.find({}, {limit: 10, sort: {timestamp: -1}});
 });
 Meteor.publish("players", function() {
-	return Players.find({}, {limit: 10, sort: {score: -1, name: 1}});
+    return Players.find({}, {limit: 10, sort: {score: -1, name: 1}});
 });
 
 // Method implementations, called asynchronously from the client
 // (for the client, the stubs are declared in /client/methods.js)
 Meteor.methods({
-	// pick a random question out of the Questions collection/DB
+    // pick a random question out of the Questions collection/DB
     getRandomQuestion: function() {
         var q = Questions.find().fetch();
         q = _.shuffle(q);
         return q[0];
     },
-	// write the log entry with the name, score and message given,
-	// extend with the current timestamp (makes use of momentJS library in the ./lib folder)
+    // write the log entry with the name, score and message given,
+    // extend with the current timestamp (makes use of momentJS library in the ./lib folder)
     writeLog: function(theName, theScore, theMessage) {
         var dateTimeString = moment().format("YYYY-MM-DD HH:mm:ss");
         Logs.insert({timestamp: dateTimeString, name: theName, score: theScore, message: theMessage});
